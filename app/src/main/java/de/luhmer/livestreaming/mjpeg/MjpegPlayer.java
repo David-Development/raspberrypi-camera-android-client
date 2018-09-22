4fe7ffa691f3acda01268626a2121084993cf2e5
David Luhmer <david-dev@live.de>
2018-09-22 10:22:40 +0200

    remove unused mjpeg player code

--- app/src/main/java/de/luhmer/livestreaming/mjpeg/MjpegPlayer.java
+++
@@ -1,8 +0,0 @@
-package de.luhmer.livestreaming.mjpeg;
-
-/**
- * Created by david on 10.07.17.
- */
-
-public class MjpegPlayer {
-}
--- app/src/main/java/de/luhmer/livestreaming/mjpeg/OnFrameCapturedListener.java
+++
@@ -1,7 +0,0 @@
-package de.luhmer.livestreaming.mjpeg;
-
-import android.graphics.Bitmap;
-
-public interface OnFrameCapturedListener {
-    void onFrameCaptured(Bitmap bitmap);
-}
